11524b9c65bc2830cbefa66228a1a7beb5dfe383
Azaan Khan <64011339+AzaanD1@users.noreply.github.com>
2020-04-29 20:13:44 +0530

    Update tic tac toe_complete (1).py

--- tic tac toe_complete (1).py
+++ tic tac toe_complete (1).py
@@ -1,3 +1,4 @@
+#hello frands im aditya p
 #tic tac toe game
 #after you get 3 in a row, click on the X/O tile again to display win message box
 #EDIT(17/10/2017): Added scoreboard and resetting of game
@@ -21,10 +22,10 @@ draw = 0
 def board(buttons):
    global bclick
    if buttons["text"] == " " and bclick == True:
-       buttons["text"] = "O"
+       buttons["text"] = "F"
        bclick = False
    elif buttons["text"] == " " and bclick == False:
-       buttons["text"] = "X"
+       buttons["text"] = "P"
        bclick = True
        
 #now for win situations
@@ -32,15 +33,15 @@ def board(buttons):
 #so , the below codes check for any of the situations for X or O
       
 #situation if player X wins
-   elif (button1["text"] == "X" and button2["text"] == "X" and button3["text"] == "X" or
-       button4["text"] == "X" and button5["text"] == "X" and button6["text"] == "X" or
-       button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X" or
-       button1["text"] == "X" and button4["text"] == "X" and button7["text"] == "X" or
-       button2["text"] == "X" and button5["text"] == "X" and button8["text"] == "X" or
-       button3["text"] == "X" and button6["text"] == "X" and button9["text"] == "X" or
-       button1["text"] == "X" and button5["text"] == "X" and button9["text"] == "X" or
-       button3["text"] == "X" and button5["text"] == "X" and button7["text"] == "X" ):
-            tk.messagebox.showinfo("Winner X", "Player X won the game")
+   elif (button1["text"] == "P" and button2["text"] == "P" and button3["text"] == "P" or
+       button4["text"] == "P" and button5["text"] == "P" and button6["text"] == "P" or
+       button7["text"] == "P" and button8["text"] == "P" and button9["text"] == "P" or
+       button1["text"] == "P" and button4["text"] == "P" and button7["text"] == "P" or
+       button2["text"] == "P" and button5["text"] == "P" and button8["text"] == "P" or
+       button3["text"] == "P" and button6["text"] == "P" and button9["text"] == "P" or
+       button1["text"] == "P" and button5["text"] == "P" and button9["text"] == "P" or
+       button3["text"] == "P" and button5["text"] == "P" and button7["text"] == "P" ):
+            tk.messagebox.showinfo("STOP PLAYING BY URSELF", "GO MAKE FRIENDZZZ")
             button1["text"] = " "
             button2["text"] = " "
             button3["text"] = " "
@@ -52,17 +53,17 @@ def board(buttons):
             button9["text"] = " "
             global Xwins
             Xwins = Xwins + 1
-            Xscoreboard["text"] = "X =", Xwins
+            Xscoreboard["text"] = "P =", Xwins
 #situation if player O wins
-   elif (button1["text"] == "O" and button2["text"] == "O" and button3["text"] == "O" or
-        button4["text"] == "O" and button5["text"] == "O" and button6["text"] == "O" or
-        button7["text"] == "O" and button8["text"] == "O" and button9["text"] == "O" or
-        button1["text"] == "O" and button4["text"] == "O" and button7["text"] == "O" or
-        button2["text"] == "O" and button5["text"] == "O" and button8["text"] == "O" or
-        button3["text"] == "O" and button6["text"] == "O" and button9["text"] == "O" or
-        button1["text"] == "O" and button5["text"] == "O" and button9["text"] == "O" or
-        button3["text"] == "O" and button5["text"] == "O" and button7["text"] == "O" ):
-             tk.messagebox.showinfo("Winner O", "Player O won the game")
+   elif (button1["text"] == "F" and button2["text"] == "F" and button3["text"] == "F" or
+        button4["text"] == "F" and button5["text"] == "F" and button6["text"] == "F" or
+        button7["text"] == "F" and button8["text"] == "F" and button9["text"] == "F" or
+        button1["text"] == "F" and button4["text"] == "F" and button7["text"] == "F" or
+        button2["text"] == "F" and button5["text"] == "F" and button8["text"] == "F" or
+        button3["text"] == "F" and button6["text"] == "F" and button9["text"] == "F" or
+        button1["text"] == "F" and button5["text"] == "F" and button9["text"] == "F" or
+        button3["text"] == "F" and button5["text"] == "F" and button7["text"] == "F" ):
+             tk.messagebox.showinfo("BRUH IK U MADE F WIN ON PURPOUSE UGH", "UR PLAYIN' ALONE BRUH")
              button1["text"] = " "
              button2["text"] = " "
              button3["text"] = " "
@@ -74,24 +75,24 @@ def board(buttons):
              button9["text"] = " "
              global Owins                       
              Owins = Owins + 1
-             Oscoreboard["text"] = "O =", Owins
-   elif (button1["text"] != "X" or button2["text"] != "X" or button3["text"] != "X" and
-       button4["text"] != "X" or button5["text"] != "X" or button6["text"] != "X" and
-       button7["text"] != "X" or button8["text"] != "X" or button9["text"] != "X" and
-       button1["text"] != "X" or button4["text"] != "X" or button7["text"] != "X" and
-       button2["text"] != "X" or button5["text"] != "X" or button8["text"] != "X" and
-       button3["text"] != "X" or button6["text"] != "X" or button9["text"] != "X" and
-       button1["text"] != "X" or button5["text"] != "X" or button9["text"] != "X" and
-       button3["text"] != "X" or button5["text"] != "X" or button7["text"] != "X" and
-       button1["text"] != "O" or button2["text"] != "O" or button3["text"] != "O" and
-       button4["text"] != "O" or button5["text"] != "O" or button6["text"] != "O" and
-       button7["text"] != "O" or button8["text"] != "O" or button9["text"] != "O" and
-       button1["text"] != "O" or button4["text"] != "O" or button7["text"] != "O" and
-       button2["text"] != "O" or button5["text"] != "O" or button8["text"] != "O" and
-       button3["text"] != "O" or button6["text"] != "O" or button9["text"] != "O" and
-       button1["text"] != "O" or button5["text"] != "O" or button9["text"] != "O" and
-       button3["text"] != "O" or button5["text"] != "O" or button7["text"] != "O" ):
-              tk.messagebox.showinfo("DRAW", "Well played!")
+             Oscoreboard["text"] = "F =", Owins
+   elif (button1["text"] != "P" or button2["text"] != "P" or button3["text"] != "P" and
+       button4["text"] != "P" or button5["text"] != "P" or button6["text"] != "P" and
+       button7["text"] != "P" or button8["text"] != "P" or button9["text"] != "P" and
+       button1["text"] != "P" or button4["text"] != "P" or button7["text"] != "P" and
+       button2["text"] != "P" or button5["text"] != "P" or button8["text"] != "P" and
+       button3["text"] != "P" or button6["text"] != "P" or button9["text"] != "P" and
+       button1["text"] != "P" or button5["text"] != "P" or button9["text"] != "P" and
+       button3["text"] != "P" or button5["text"] != "P" or button7["text"] != "P" and
+       button1["text"] != "F" or button2["text"] != "F" or button3["text"] != "F" and
+       button4["text"] != "F" or button5["text"] != "F" or button6["text"] != "F" and
+       button7["text"] != "F" or button8["text"] != "F" or button9["text"] != "F" and
+       button1["text"] != "F" or button4["text"] != "F" or button7["text"] != "F" and
+       button2["text"] != "F" or button5["text"] != "F" or button8["text"] != "F" and
+       button3["text"] != "F" or button6["text"] != "F" or button9["text"] != "F" and
+       button1["text"] != "F" or button5["text"] != "F" or button9["text"] != "F" and
+       button3["text"] != "F" or button5["text"] != "F" or button7["text"] != "F" ):
+              tk.messagebox.showinfo("SUCH A NOOB...", "ENDED IN A DRAW BY URSELF UGH... STOP PLAYING ALONE GO MAKE FRENDS")
               button1["text"] = " "
               button2["text"] = " "
               button3["text"] = " "
